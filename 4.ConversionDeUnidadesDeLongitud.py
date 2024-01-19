aa019eba0be7c7a816a27764d091190fcc269931
eduardo608 <72846380+eduardo608@users.noreply.github.com>
2024-01-19 12:12:45 -0500

    EjerciCIO 4 Conversion de Unidades

---
+++ 4.ConversionDeUnidadesDeLongitud.py
@@ -0,0 +1,7 @@
+#Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+
+longitud = float(input("Ingrese la Longitud"))
+
+pulgada = longitud / 2.54
+
+print(f"{longitud} cm: {pulgada} in")
